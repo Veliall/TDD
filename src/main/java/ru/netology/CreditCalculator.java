6f5de13e4a8bd482eff56c3cda85b67cd84ca020
Igor Khristiuk <ikhristiuk@gmail.com>
2021-09-10 15:05:47 +0300

    Methods created

--- src/main/java/ru/netology/CreditCalculator.java
+++ src/main/java/ru/netology/CreditCalculator.java
@@ -2,6 +2,29 @@ package ru.netology;
 
 public class CreditCalculator {
 
-    public static void main(String[] args) {
+    public double getTotalAmount(final long amount, final int percent, final int months) {
+        if (amount <= 0 || percent <= 0 || months <= 0) {
+            throw new IllegalArgumentException("Incorrect data");
+        }
+
+        return (getOverpayment(amount, percent, months) + amount);
+    }
+
+    public double getOverpayment(final long amount, final int percent, final int months) {
+        if (amount <= 0 || percent <= 0 || months <= 0) {
+            throw new IllegalArgumentException("Incorrect data");
+        }
+        int countOfYears = months / 12;
+        if (months % 12 != 0) countOfYears++;
+
+        return (percent * countOfYears * amount / 100) / 10.0;
+    }
+
+    public double getPaymentPerMonth(final long amount, final int percent, final int months) {
+        if (amount <= 0 || percent <= 0 || months <= 0) {
+            throw new IllegalArgumentException("Incorrect data");
+        }
+
+        return getTotalAmount(amount, percent, months) / months;
     }
 }
